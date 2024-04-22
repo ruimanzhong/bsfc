5a5eab617c77725f30a024bab08922314fff00eb
RuimanZhong <ruiman.zhong@gmail.com>
2024-04-22 18:33:29 +0300

    correct mlik_corrected()

--- R/within_clust_model.R
+++ R/within_clust_model.R
@@ -111,7 +111,7 @@ prepare_data_each <- function(k, Y, membership, X = NULL, N = NULL) {
 ## Auxiliary function to correct the marginal likelihood of INLA model
 
 mlik_corrected <- function(inla_model) {
-  theta <- exp(as.numeric(inla_model[["misc"]][["configs"]][["config"]][[1]][["theta"]][[1]]))
+  theta <- exp(as.numeric(inla_model[["misc"]][["configs"]][["config"]][[1]][["theta"]][[2]]))
   Q <- inla_model[["misc"]][["configs"]][["config"]][[1]][["Qprior"]]
   dim <- dim(Q)[1] - 1
   det <- sum(diag(as.matrix(SparseM::chol(Q[1:dim,1:dim])))^2)
